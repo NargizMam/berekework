2315a8829e0bd3f51663987ae905611fc492c45f
Zhuravlev Petr <zeterfrost@gmail.com>
2024-04-08 23:15:55 +0600

    created LinkButtonWithArrow

---
+++ frontend/src/shared/linkButtonWithArrow/index.ts
@@ -0,0 +1,1 @@
+export { LinkButtonWithArrow } from './ui/LinkButtonWithArrow';
---
+++ frontend/src/shared/linkButtonWithArrow/ui/LinkButtonWithArrow.tsx
@@ -0,0 +1,25 @@
+import { Link, Box, Typography } from '@mui/material';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+import Arrow from '../../../shared/assets/icons/Arrow-Right.png';
+import LinkButtonWithArrowStyle from './LinkButtonWithArrow-style';
+import './LinkButtonWithArrow.css';
+
+interface Props {
+  url: string;
+  text: string;
+}
+
+export const LinkButtonWithArrow: React.FC<Props> = ({ url, text }) => {
+  return (
+    <>
+      <Link component={RouterLink} to={url} sx={LinkButtonWithArrowStyle.text}>
+        <Box sx={LinkButtonWithArrowStyle.style}>
+          <Typography>{text}</Typography>
+          <img src={Arrow} alt={text} className="LinkButtonWithArrow__arrow" />
+        </Box>
+      </Link>
+    </>
+  );
+};
