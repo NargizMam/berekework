248455d7053ead477066beb7157ee9ea832202c1
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-04-08 19:22:23 +0600

    add field

--- api/routers/headingRouter.ts
+++ api/routers/headingRouter.ts
@@ -8,9 +8,12 @@ headingRouter.post('/', imagesUpload.single('image'), async (req, res, next) => 
   try {
     const heading = new Heading({
       title: req.body.title,
-      url: req.body.url,
       image: req.file ? req.file.filename : null,
       description: req.body.description,
+      button: {
+        url: req.body.url,
+        text: req.body.text,
+      },
     });
     await heading.save();
     return res.send(heading);
@@ -23,6 +26,20 @@ headingRouter.get('/', async (_req, res, next) => {
   try {
     const results = await Heading.findOne();
     return res.send(results);
+  } catch (error) {
+    return next(error);
+  }
+});
+
+headingRouter.patch('/', imagesUpload.single('image'), async (req, res, next) => {
+  try {
+    const result = await Heading.updateOne({
+      title: req.body.title,
+      image: req.file ? req.file.filename : null,
+      description: req.body.description,
+      button: JSON.parse(req.body.button),
+    });
+    return res.send(result);
   } catch (error) {
     return next(error);
   }
