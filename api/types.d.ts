865fc128bbea2e4c118fc811d31ccb92d7719063
Susanna Andreeva <sanadex90@gmail.com>
2024-04-12 14:13:32 +0900

    fix types.d.ts

--- api/types.d.ts
+++ api/types.d.ts
@@ -9,25 +9,7 @@ export interface Vacancy {
   salaryMax: number | null;
 }
 
-export interface VacancyMutation {
-  title: string;
-  description: string;
-  companyLogo?: string | null;
-  companyName: string;
-  city: string;
-  salaryMin: number | null;
-  salaryMax: number | null;
-}
+export type VacancyMutation = Omit<Vacancy, '_id'>;
 
-export interface NavbarItemFields {
-  nameNav: string;
-  link: string;
-  isDrop: boolean;
-  access: string;
-  nestedMenu: [
-    {
-      nestedNameNav: string;
-      nestedLink: string;
-    },
-  ];
-}
+
+
