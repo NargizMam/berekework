92ee2d348c230de7b4c7cb3b3eb8f2252f40414c
Susanna Andreeva <sanadex90@gmail.com>
2024-04-16 20:38:04 +0900

    delete PATCH, DELETE, GET by ID methods to vacanciesBlockRouter

--- api/routers/vacanciesBlockRouter.ts
+++ api/routers/vacanciesBlockRouter.ts
@@ -1,7 +1,7 @@
-import mongoose, { Types } from 'mongoose';
+import mongoose from 'mongoose';
 import express from 'express';
 import VacanciesBlock from '../models/VacanciesBlock';
-import { VacanciesBlockMutation } from '../types';
+import {VacanciesBlockMutation} from '../types';
 
 const vacanciesBlockRouter = express.Router();
 
@@ -38,91 +38,6 @@ vacanciesBlockRouter.get('/', async (req, res, next) => {
 
     return res.send(result);
   } catch (e) {
-    next(e);
-  }
-});
-
-vacanciesBlockRouter.get('/:id', async (req, res, next) => {
-  try {
-    let _id: Types.ObjectId;
-    try {
-      _id = new Types.ObjectId(req.params.id);
-    } catch {
-      return res.status(404).send({ error: 'Wrong ObjectId!' });
-    }
-
-    const vacanciesBlock = await VacanciesBlock.findById(_id);
-
-    if (!vacanciesBlock) {
-      return res.status(404).send({ error: 'Vacancies block not found!' });
-    }
-
-    res.send(vacanciesBlock);
-  } catch (e) {
-    next(e);
-  }
-});
-
-vacanciesBlockRouter.patch('/:id', async (req, res, next) => {
-  const { title, cards, button, location } = req.body;
-
-  try {
-    const _id = req.params.id;
-
-    const existedVacanciesBlock = await VacanciesBlock.findById(_id);
-
-    if (!existedVacanciesBlock) {
-      return res.status(404).send({ error: 'Vacancies block not found' });
-    }
-
-    Object.assign(existedVacanciesBlock, {
-      title,
-      button,
-      location,
-    });
-
-    await existedVacanciesBlock.save();
-
-    return res.send({
-      message: 'VacanciesBlock has been changed',
-      existedVacanciesBlock,
-    });
-  } catch (e) {
-    if (e instanceof mongoose.Error.ValidationError) {
-      return res.status(422).send(e);
-    }
-
-    if (e instanceof mongoose.Error.CastError) {
-      return res.status(400).send({ message: 'Invalid ID' });
-    }
-
-    next(e);
-  }
-});
-
-vacanciesBlockRouter.delete('/:id', async (req, res, next) => {
-  try {
-    let _id: Types.ObjectId;
-    try {
-      _id = new Types.ObjectId(req.params.id);
-    } catch {
-      return res.status(404).send({ error: 'Wrong ObjectId!' });
-    }
-
-    const result = await VacanciesBlock.findByIdAndDelete(_id);
-
-    if (!result) {
-      return res.status(404).send({
-        error: 'Vacancies block not found or already deleted',
-      });
-    }
-
-    return res.send({ message: 'success', result });
-  } catch (e) {
-    if (e instanceof mongoose.Error.CastError) {
-      return res.status(400).send({ error: 'Invalid ID' });
-    }
-
     next(e);
   }
 });
