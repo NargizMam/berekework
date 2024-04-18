1b9b6c811766a50eaf546df6850e9f1589636c17
rdb03 <rdb03@mail.ru>
2024-04-18 19:17:54 +0600

    finalized the footer

---
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+import Footer from '../../widgets/footer/ui/Footer';
+
+const HomePage = () => {
+  return (
+    <>
+      <footer>
+        <Footer/>
+      </footer>
+    </>
+  );
+};
+
+export default HomePage;
