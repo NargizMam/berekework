f6da7828d01c11ce3ef7e17e9d629dd73b780bb7
Nargiz Mamytova <mam.zigran@gmail.com>
2024-04-28 14:23:22 +0600

    SideNavbars button && Routes.

--- frontend/src/app/layouts/adminLayout/MainNavAdmin.tsx
+++ frontend/src/app/layouts/adminLayout/MainNavAdmin.tsx
@@ -1,56 +1,58 @@
 import * as React from 'react';
+import { useEffect, useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
 import ListIcon from '@mui/icons-material/List';
-import SideNavAdmin from './SideNavAdmin';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { SideNavAdmin } from './SideNavAdmin';
 
 const MainNavAdmin = (): React.JSX.Element => {
-  const [openNav, setOpenNav] = React.useState<boolean>(false);
+  const theme = useTheme();
+  const isMobileScreen = useMediaQuery(theme.breakpoints.down('md'));
 
+  const [openNav, setOpenNav] = useState<boolean>(false);
 
-  let sideNavBar = <SideNavAdmin open={true}/>;
+  const handleNavToggle = () => {
+    setOpenNav((prev) => !prev);
+  };
 
-  if (window.innerWidth < 1000) {
-    sideNavBar = <SideNavAdmin open={openNav} onClose={() => setOpenNav(false)}/>;
-  }
-  React.useEffect(() => {
-    const handleResize = () => {
-      setOpenNav(window.innerWidth < 1000);
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  useEffect(() => {
+    if (!isMobileScreen) {
+      setOpenNav(true);
+    }
+  }, [isMobileScreen]);
 
   return (
-    <>
-      <Box component="header" display='flex'>
-        <Stack
-          direction="row"
-          spacing={2}
-          sx={{alignItems: 'center', justifyContent: 'flex-start', minHeight: '64px', px: 2}}
-        >
+    <Box component="header" display='flex' position="relative">
+      <Stack
+        direction="row"
+        spacing={2}
+        sx={{alignItems: 'center', justifyContent: 'flex-start', minHeight: '64px', px: 2}}
+      >
+        {isMobileScreen && (
           <IconButton
-            onClick={(): void => {
-              setOpenNav(true);
-            }}
-            sx={{display: {lg: 'none'}}}
+            onClick={handleNavToggle}
+            sx={{ display: { lg: 'none' } }}
           >
-            <ListIcon/>
+            <ListIcon />
           </IconButton>
-        </Stack>
-        {sideNavBar}
-        <Stack sx={{alignItems: 'center', justifyContent: 'flex-end', flexGrow: 1}} direction="row" spacing={5}>
-          <Avatar src="/assets/avatar.png" sx={{cursor: 'pointer'}}/>
-        </Stack>
-      </Box>
-    </>
+        )}
+
+      </Stack>
+      {isMobileScreen ? (
+        <SideNavAdmin open={openNav} onClose={() => setOpenNav(false)} />
+      ) : (
+        <SideNavAdmin open={true}/>
+      )}
+      <Stack sx={{alignItems: 'center', justifyContent: 'flex-end', flexGrow: 1}} direction="row" spacing={5}>
+        <Avatar src="/assets/avatar.png" sx={{cursor: 'pointer'}}/>
+      </Stack>
+    </Box>
   );
 };
 
 export default MainNavAdmin;
+
