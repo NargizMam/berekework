206b6f6cb5da5c37dbe70583afe06f44015ccd3f
rdb03 <rdb03@mail.ru>
2024-05-01 19:35:56 +0600

    fix dev 1

--- frontend/src/client/page/HomePage/HomePage.tsx
+++ frontend/src/client/page/HomePage/HomePage.tsx
@@ -1,15 +1,30 @@
 import Footer from '../../widgets/footer/ui/Footer';
 import { MainCards } from '../../../admin/widgets/mainCards';
 import LastNewsBlock from '../../widgets/lastNewsBlock/ui/LastNewsBlock';
+import Header from '../../../widgets/Header/ui/Header';
+import { TitleBlock } from '../../../admin/widgets/titleBlock';
 
 const HomePage = () => {
-
   return (
     <>
-      <MainCards />
-      <LastNewsBlock />
+      <header>
+        <Header/>
+      </header>
+      <main>
+        <MainCards/>
+        <LastNewsBlock/>
+        <TitleBlock data={{
+          title: 'Найди работу, которая делает каждый день интересным',
+          location: 'kljlk',
+          button: {
+            url: '/',
+            text: 'Перейти к вакансиям'
+          }
+        }}/>
+
+      </main>
       <footer>
-        <Footer />
+        <Footer/>
       </footer>
     </>
   );
