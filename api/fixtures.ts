f2017fa2e2c02e21db481c7c4270ec663e476d3e
Nargiz Mamytova <mam.zigran@gmail.com>
2024-05-05 17:44:58 +0600

    Employers fixtures

--- api/fixtures.ts
+++ api/fixtures.ts
@@ -131,49 +131,52 @@ const run = async () => {
       },
   );
 
-  // await Employer.create({
-  //   email: 'employer@gmail.com',
-  //   password: 'employer',
-  //   token: randomUUID(),
-  //   role: 'employer',
-  // });
-  //
-  // await Tariff.create({
-  //   mainTitle: 'Tariff',
-  //   title: 'Basic',
-  //   description: ['Free Food', 'Apple Music'],
-  // });
-  //
-  // await LastNewsBlock.create({
-  //   title: 'Последние новости',
-  //   page: 'last-news-block',
-  //   cards: [
-  //     {
-  //       cardTitle: 'Природные катастрофы угрожают',
-  //       cardText: 'Извержения вулканов и землетрясения: что делать и как подготовиться?',
-  //       dateTime: '2024-04-21T12:00:00Z',
-  //       buttonUrl: '/natural-disasters',
-  //     },
-  //     {
-  //       cardTitle: 'Рост напряженности на Украине',
-  //       cardText: 'Международные обсуждения и реакции на политическую ситуацию',
-  //       dateTime: '2024-04-21T12:00:00Z',
-  //       buttonUrl: '/ukraine-tensions',
-  //     },
-  //     {
-  //       cardTitle: 'Экономические прогнозы на следующий квартал',
-  //       cardText: 'Какие изменения ожидаются в мировой экономике и на рынках?',
-  //       dateTime: '2024-04-21T12:00:00Z',
-  //       buttonUrl: '/economic-forecasts',
-  //     },
-  //     {
-  //       cardTitle: 'Новые технологии в медицине',
-  //       cardText: 'Искусственный интеллект, биотехнологии и перспективы лечения заболеваний',
-  //       dateTime: '2024-04-21T12:00:00Z',
-  //       buttonUrl: '/medical-technologies',
-  //     },
-  //   ],
-  // });
+  await Employer.create({
+    email: 'employer@gmail.com',
+    companyName: 'HeadeHunter',
+    scope: 'Биржа',
+    foundationYear: '1999',
+    password: 'employer',
+    token: randomUUID(),
+    role: 'employer',
+  });
+
+  await Tariff.create({
+    mainTitle: 'Tariff',
+    title: 'Basic',
+    description: ['Free Food', 'Apple Music'],
+  });
+
+  await LastNewsBlock.create({
+    title: 'Последние новости',
+    page: 'last-news-block',
+    cards: [
+      {
+        cardTitle: 'Природные катастрофы угрожают',
+        cardText: 'Извержения вулканов и землетрясения: что делать и как подготовиться?',
+        dateTime: '2024-04-21T12:00:00Z',
+        buttonUrl: '/natural-disasters',
+      },
+      {
+        cardTitle: 'Рост напряженности на Украине',
+        cardText: 'Международные обсуждения и реакции на политическую ситуацию',
+        dateTime: '2024-04-21T12:00:00Z',
+        buttonUrl: '/ukraine-tensions',
+      },
+      {
+        cardTitle: 'Экономические прогнозы на следующий квартал',
+        cardText: 'Какие изменения ожидаются в мировой экономике и на рынках?',
+        dateTime: '2024-04-21T12:00:00Z',
+        buttonUrl: '/economic-forecasts',
+      },
+      {
+        cardTitle: 'Новые технологии в медицине',
+        cardText: 'Искусственный интеллект, биотехнологии и перспективы лечения заболеваний',
+        dateTime: '2024-04-21T12:00:00Z',
+        buttonUrl: '/medical-technologies',
+      },
+    ],
+  });
 
   await db.close();
 };
