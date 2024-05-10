c576b0928b85499d2921bdf98cf4e76f955aeb67
Susanna Andreeva <sanadex90@gmail.com>
2024-05-10 17:17:57 +0900

    fix MainCardItem with image for mobile

--- frontend/src/admin/widgets/mainCards/ui/MainCardItem-style.ts
+++ frontend/src/admin/widgets/mainCards/ui/MainCardItem-style.ts
@@ -1,50 +1,50 @@
-const MainCardItemStyle = (cardIcon: string | null) => ({
-  card: {
-    display: 'flex',
-    maxWidth: '100%',
-    flexBasis: 'calc(50% - 5px)',
-    borderRadius: '30px',
-    alignItems: 'center',
-    textDecoration: 'none',
-    background: '#ECECEC',
-    border: '1px solid #ECECEC',
-    overflow: 'hidden',
-    padding: cardIcon ? '40px 40px 40px 124px' : '50px 250px 48px 50px',
-  },
-  title: {
-    fontSize: '20px',
-    fontWeight: '600',
-    color: '#000',
-    lineHeight: 1.1,
-    margin: '0 0 20px 0',
-  },
-  text: {
-    fontSize: '14px',
-    fontWeight: '500',
-    lineHeight: 1.4,
-    color: '#777',
-    margin: 0,
-  },
-  icon: {
-    maxWidth: '24px',
-    maxHeight: '24px',
-    '@media (min-width: 600px)': {
-      left: '40px',
-      top: '64px',
-      maxWidth: '54px',
-      maxHeight: '54px',
-    },
-  },
-  image: {
-    width: '228px',
-    height: '228px',
-    transform: 'rotate(-12.28deg)',
-    bottom: '-54px',
-    right: '-60px',
-  },
-  link: {
-    textDecoration: 'none',
-  },
-});
+// const MainCardItemStyle = (cardIcon: string | null) => ({
+//   card: {
+//     // display: 'flex',
+//     // maxWidth: '100%',
+//     // flexBasis: 'calc(50% - 5px)',
+//     // borderRadius: '30px',
+//     // alignItems: 'center',
+//     // textDecoration: 'none',
+//     // background: '#ECECEC',
+//     // border: '1px solid #ECECEC',
+//     // overflow: 'hidden',
+//     // padding: cardIcon ? '40px 40px 40px 124px' : '50px 250px 48px 50px',
+//   },
+//   // title: {
+//   //   fontSize: '20px',
+//   //   fontWeight: '600',
+//   //   color: '#000',
+//   //   lineHeight: 1.1,
+//   //   margin: '0 0 20px 0',
+//   // },
+//   // text: {
+//   //   fontSize: '14px',
+//   //   fontWeight: '500',
+//   //   lineHeight: 1.4,
+//   //   color: '#777',
+//   //   margin: 0,
+//   // },
+//   // icon: {
+//   //   maxWidth: '24px',
+//   //   maxHeight: '24px',
+//   //   '@media (min-width: 600px)': {
+//   //     left: '40px',
+//   //     top: '64px',
+//   //     maxWidth: '54px',
+//   //     maxHeight: '54px',
+//   //   },
+//   // },
+//   // image: {
+//   //   width: '228px',
+//   //   height: '228px',
+//   //   transform: 'rotate(-12.28deg)',
+//   //   bottom: '-54px',
+//   //   right: '-60px',
+//   // },
+//   // link: {
+//   //   textDecoration: 'none',
+//   // },
+// });
 
-export default MainCardItemStyle;
+// export default MainCardItemStyle;
--- frontend/src/admin/widgets/mainCards/ui/MainCardItem.tsx
+++ frontend/src/admin/widgets/mainCards/ui/MainCardItem.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import MainCardItemStyle from './MainCardItem-style';
 import { API_URL } from '../../../../app/constants/links';
 import './MainCardItem.css';
 
@@ -17,14 +16,18 @@ const MainCardItem: React.FC<Props> = ({ title, text, image, icon, URLpath }) =>
   const cardIcon = icon ? API_URL + icon : null;
   const cardImage = image ? API_URL + image : null;
 
-  const styles = MainCardItemStyle(cardIcon);
+  // const styles = MainCardItemStyle(cardIcon);
 
   const iconElement = cardIcon ? <img className="MainCardItem__card-icon" src={cardIcon} alt={title} /> : null;
 
-  const imageElement = cardImage ? <img className="MainCardItem__card-image" src={cardImage} alt={title} /> : null;
+  const imageElement = cardImage ? (
+    <div className="MainCardItem__image-wrapper">
+      <img className="MainCardItem__card-image" src={cardImage} alt={title} />
+    </div>
+  ) : null;
 
   const cardContent = (
-    <div>
+    <div className="MainCardItem__card-content">
       <h5 className="MainCardItem__card-title">{title}</h5>
       <p className="MainCardItem__card-text">{text}</p>
     </div>
@@ -33,27 +36,25 @@ const MainCardItem: React.FC<Props> = ({ title, text, image, icon, URLpath }) =>
   const card = (
     <>
       {cardImage ? (
-        <>
+        <div className="MainCardItem__cardWithImage">
           {cardContent}
           {imageElement}
-        </>
+        </div>
       ) : (
-        <>
+        <div className="MainCardItem__cardWithIcon">
           {iconElement}
           {cardContent}
-        </>
+        </div>
       )}
     </>
   );
 
   return URLpath ? (
-    <Link to={URLpath} style={styles.card} className="MainCardItem__card">
+    <Link to={URLpath} className="MainCardItem__card-link">
       {card}
     </Link>
   ) : (
-    <div style={styles.card} className="MainCardItem__card">
-      {card}
-    </div>
+    <>{card}</>
   );
 };
 
