846d7feefb5dd5b53f75bd0d16e7792d5b21e337
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-05-10 20:47:41 +0600

    add model Vacancy

--- api/models/vacancy/Vacancy.ts
+++ api/models/vacancy/Vacancy.ts
@@ -1,30 +1,74 @@
-import {model, Schema} from 'mongoose';
+import { model, Schema } from 'mongoose';
 
 const vacancySchema = new Schema(
   {
-    title: {
+    // Значение которое будет в карточке
+    logoCompany: {
       type: String,
       required: true,
     },
-    description: {
+    nameCompany: {
       type: String,
+      required: true,
+    },
+    vacancyTitle: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      minSalary: {
+        type: Number,
+        required: true,
+      },
+      maxSalary: {
+        type: Number,
+        required: true,
+      },
     },
     city: {
       type: String,
       required: true,
     },
-    salary: {
-      min: Number,
-      max: Number,
-    },
-    url: {
+    aboutVacancy: { // О вакансии (минимум 200 символов)
       type: String,
     },
-  employer: {
+    responsibilities: {// Обязанности (минимум 150 символов)
+      type: String,
+    },
+    workConditions: {  // Условия работы (Минимум 100 символов)
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    fieldOfWork: { // Сфера деятельности
+      type: String,
+    },
+    age: { // Возрастная категория (17-30, 30-45, 45+
+      minAge: {
+        type: Number,
+      },
+      maxAge: {
+        type: Number,
+      },
+    },
+    education: { // Образование (среднее, средне-специальное, высшее, высшее-неполное магистратура, без образования)
+      type: String,
+    },
+    employmentType: {  // Тип занятости: Полная, Неполная, Подработка
+      type: String,
+    },
+    /*users: [
+      {
+        userId: ref - на соискателя
+        chosenBy: Boolean, // true - Когда соискатель сам выбрал
+      }                    // false - когда рабодатель выбрал себе соискателя
+    ],*/
+    employer: {
       type: Schema.Types.ObjectId,
       ref: 'Employer',
       required: true,
-  },
+    },
   },
   { timestamps: true },
 );
@@ -32,3 +76,4 @@ const vacancySchema = new Schema(
 const Vacancy = model('Vacancy', vacancySchema);
 
 export default Vacancy;
+
