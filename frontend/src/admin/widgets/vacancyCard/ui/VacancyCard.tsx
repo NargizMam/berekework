1b78063b62a1a544ecb6fa4219691796d9618bcc
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-17 19:00:46 +0600

    fixed logic in VacaancyCard component

--- frontend/src/admin/widgets/vacancyCard/ui/VacancyCard.tsx
+++ frontend/src/admin/widgets/vacancyCard/ui/VacancyCard.tsx
@@ -2,35 +2,35 @@ import { Box, Typography } from '@mui/material';
 import React from 'react';
 import VacancyCardStyle from './VacancyCard-style';
 import './VacancyCard.css';
-import { VacancyCardApiData } from '../../../../shared/types';
+import { VacancyCardApiData } from '../../vacancyBlock/types';
 
 interface Props {
   data: VacancyCardApiData;
 }
 
 export const VacancyCard: React.FC<Props> = ({ data }) => {
-  const image = data.logo ? (
+  const image = data.logoCompany ? (
     <Box sx={VacancyCardStyle.imageWrapper}>
-      <img src={data.logo} alt={data.title} />
+      <img src={data.logoCompany} alt={data.vacancyTitle} />
     </Box>
   ) : null;
   let salary = 'з/п не указана';
 
-  if (data.salary?.min && data.salary.max) {
-    salary = `от ${data.salary.min} до ${data.salary.max} сом`;
-  } else if (data.salary?.min) {
-    salary = `от ${data.salary.min} сом`;
-  } else if (data.salary?.max) {
-    salary = `до ${data.salary.max} сом`;
+  if (data.salary?.minSalary && data.salary.maxSalary) {
+    salary = `от ${data.salary.minSalary} до ${data.salary.maxSalary} сом`;
+  } else if (data.salary?.minSalary) {
+    salary = `от ${data.salary.minSalary} сом`;
+  } else if (data.salary?.maxSalary) {
+    salary = `до ${data.salary.maxSalary} сом`;
   }
   return (
     <div className="VacancyCard">
       {image}
       <Typography variant="h5" sx={VacancyCardStyle.title}>
-        {data.title}
+        {data.vacancyTitle}
       </Typography>
       <Typography sx={VacancyCardStyle.subTitle}>
-        {data.company}, {data.city}
+        {data.nameCompany}, {data.city}
       </Typography>
       <Typography variant="h5" sx={VacancyCardStyle.salary}>
         {salary}
