0b101b4cb2222747827c14c0f241afc5f0f06229
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-17 18:57:40 +0600

    relocated types

---
+++ frontend/src/admin/widgets/vacancyBlock/types.d.ts
@@ -0,0 +1,34 @@
+export interface VacancyBlockApiData {
+  _id: string;
+  title: string;
+  button: {
+    url: string;
+    text: string;
+  };
+  location: string;
+}
+
+export interface VacancyCardApiData {
+  _id: string;
+  vacancyTitle: string;
+  description?: string;
+  logoCompany?: string;
+  nameCompany: string;
+  country?: string;
+  city: string;
+  salary?: {
+    minSalary?: number;
+    maxSalary?: number;
+  };
+  aboutVacancy?: string;
+  responsibilities?: string;
+  workConditions?: string;
+  fieldOfWork?: string;
+  age?: {
+    minAge?: number;
+    maxAge?: number;
+  }
+  education?: string;
+  employmentType?: string;
+  employer: string;
+}
