277cc2afe2d517270eead7dd45ad56677f1b84b6
Susanna Andreeva <sanadex90@gmail.com>
2024-05-25 03:58:40 +0900

    fix MediaCard.tsx: add video cover.

--- frontend/src/client/widgets/MediaBlock/ui/MediaCard/MediaCard.tsx
+++ frontend/src/client/widgets/MediaBlock/ui/MediaCard/MediaCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Box, CardMedia, Modal } from '@mui/material';
 import ReactPlayer from 'react-player';
 import iconPlay from '../../images/icon-play.png';
@@ -21,7 +21,6 @@ const MediaCard: React.FC<MediaCardApiData> = ({ image, video }) => {
     type: 'image' | 'video';
     url: string;
   } | null>(null);
-  const [videoThumbnail, setVideoThumbnail] = useState<string | null>(null);
 
   const handleOpen = (type: 'image' | 'video', url: string) => {
     if (url) {
@@ -35,33 +34,7 @@ const MediaCard: React.FC<MediaCardApiData> = ({ image, video }) => {
     setModalContent(null);
   };
 
-  const extractVideoFrame = () => {
-    const videoElement = document.createElement('video');
-    videoElement.src = video?.url || '';
-    videoElement.crossOrigin = 'anonymous'; // Добавлено для разрешения проблем с CORS
-    videoElement.addEventListener('loadeddata', () => {
-      videoElement.currentTime = Math.random() * (videoElement.duration || 0);
-    });
-    videoElement.addEventListener('seeked', () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = videoElement.videoWidth;
-      canvas.height = videoElement.videoHeight;
-      const ctx = canvas.getContext('2d');
-      if (ctx) {
-        ctx.drawImage(videoElement, 0, 0, canvas.width, canvas.height);
-        const dataURL = canvas.toDataURL('image/png');
-        setVideoThumbnail(dataURL);
-      }
-    });
-  };
-
-  useEffect(() => {
-    if (video && !image && video.url) {
-      extractVideoFrame();
-    }
-  }, [video]);
-
-  const imageUrl = image ? image.url : videoThumbnail;
+  const imageUrl = image ? image.url : null;
   const videoUrl = video ? video.url : null;
 
   const modal = (
