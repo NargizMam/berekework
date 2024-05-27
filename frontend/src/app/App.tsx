ece7d0df8d0fb96be1cee8c3381e767472f0559a
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-05-27 20:58:59 +0600

    trash front import

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -2,14 +2,10 @@ import { Route, Routes, useLocation, useRoutes } from 'react-router-dom';
 import AdminLayout from './layouts/adminLayout/AdminLayout';
 import HomePage from '../client/page/HomePage/HomePage';
 import { AdminMainPage } from '../admin/page/adminMainPage';
-import { HeaderAdmin } from '../admin/page/headerCreate';
-import { AdminAllPages, AdminCreatePage } from '../admin/page/adminPages';
-import { HeadingAdmin, HeadingDetail } from '../admin/page/HeadingAdmin';
 import Container from '@mui/material/Container';
 import { LoginPage, RegisterPage } from '../client/page/Auth';
 import { UserPanelPage } from '../admin/page/usersPanel';
 import { VacancyPage } from '../admin/page/vacancyPanel';
-import { TariffPanelPage } from '../admin/page/tariffPanel';
 import { useAppSelector } from './store/hooks';
 import { selectUser } from '../client/page/Auth/model/AuthSlice';
 import ProtectedRoute from '../shared/ProtectedRoute/ProtectedRoute';
@@ -22,10 +18,8 @@ import ApplicantProfile from '../client/page/Applicant/ui/page/ApplicantProfile'
 import ApplicantRefactor from '../client/page/Applicant/ui/page/ApplicantRefactor';
 import { EmployerFormPage, EmployerPanelPage } from '../admin/page/employerPanel';
 import PotentialEmployeesPage from '../client/page/PotentialEmployeesPage/PotentialEmployeesPage';
-import TariffFormPage from '../admin/page/tariffPanel/ui/tariffFormPage';
 import { ForEmployerPage } from '../client/page/ForEmployerPage';
 import AboutUsPage from '../client/page/AboutUsPage/AboutUsPage';
-// import { RatesBLock } from '../client/widgets/tariff/ui/ratesBLock';
 
 
 const App = () => {
@@ -37,20 +31,12 @@ const App = () => {
       <Container>
         <Routes>
           <Route path="/" element={<AdminMainPage />} />
-          <Route path="/header" element={<HeaderAdmin />} />
-          <Route path="/pages" element={<AdminAllPages />} />
           <Route path="/moderators" element={
             <ProtectedRoute isAllowed={user?.role === 'superadmin'}>
               <ModeratorsPage/>
             </ProtectedRoute>} />
-          <Route path="/pages/new-page" element={<AdminCreatePage />} />
-          <Route path="/adminHeading" element={<HeadingAdmin />} />
-          <Route path="/adminHeading:location" element={<HeadingDetail />} />
           <Route path="/employers" element={<EmployerPanelPage/>}/>
           <Route path="/employers-submit" element={<EmployerFormPage/>}/>
-          <Route path="/tariffs" element={<TariffPanelPage/>}/>
-          <Route path="/tariffs-new" element={<TariffFormPage/>}/>
-          <Route path="/tariffs-submit/:id" element={<TariffFormPage/>}/>
           <Route path="/vacancy" element={<VacancyPage/>}/>
           <Route path="/users" element={<UserPanelPage/>}/>
         </Routes>
@@ -83,7 +69,6 @@ const App = () => {
                 <Route path="/users" element={<UserPanelPage />} />
                 <Route path="/vacancy" element={<VacancyPage />} />
                 <Route path="/about-us" element={<AboutUsPage />} />
-                {/*<Route path="/tariffs" element={<RatesBLock />} />*/}
                 <Route path="/employersProfile/:id" element={<EmployerProfile/>} />
                 <Route path="/potential-employees" element={<PotentialEmployeesPage />} />
                 <Route path="/newApplicant" element={<ApplicantSettings />} />
