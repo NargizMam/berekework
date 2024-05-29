a8b00477c6dcffac381ae11d38903615f3996790
Zhuravlev Petr <zeterfrost@gmail.com>
2024-05-29 17:16:39 +0600

    fix VacancyCard

--- frontend/src/feachers/vacancyCard/ui/VacancyCard.tsx
+++ frontend/src/feachers/vacancyCard/ui/VacancyCard.tsx
@@ -3,17 +3,40 @@ import React from 'react';
 import VacancyCardStyle from './VacancyCard-style';
 import './VacancyCard.css';
 
+// export interface VacancyCardApiData {
+//   _id: string;
+//   title: string;
+//   description?: string;
+//   logo?: string;
+//   company?: string;
+//   city: string;
+//   salary?: {
+//     min?: number;
+//     max?: number;
+//   };
+// }
 export interface VacancyCardApiData {
   _id: string;
-  title: string;
-  description?: string;
-  logo?: string;
-  company?: string;
+  logo: string;
+  company: string;
+  vacancyTitle: string;
+  aboutVacancy: string;
+  responsibilities: string;
+  workConditions: string;
+  country: string;
   city: string;
-  salary?: {
-    min?: number;
-    max?: number;
+  fieldOfWork: string;
+  salary: {
+    minSalary: number;
+    maxSalary: number;
   };
+  age: {
+    minAge: number;
+    maxAge: number;
+  };
+  education: string;
+  employmentType: string;
+  employer: string | undefined;
 }
 
 interface Props {
@@ -22,25 +45,25 @@ interface Props {
 }
 
 export const VacancyCard: React.FC<Props> = ({ data, visible }) => {
-  const image = data.logo ? (
+  const image = data.country ? (
     <Box sx={VacancyCardStyle.imageWrapper}>
-      <img src={data.logo} alt={data.title} />
+      <img src={data.logo} alt={data.vacancyTitle} />
     </Box>
   ) : null;
   let salary = 'з/п не указана';
 
-  if (data.salary?.min && data.salary.max) {
-    salary = `от ${data.salary.min} до ${data.salary.max} сом`;
-  } else if (data.salary?.min) {
-    salary = `от ${data.salary.min} сом`;
-  } else if (data.salary?.max) {
-    salary = `до ${data.salary.max} сом`;
+  if (data.salary?.minSalary && data.salary.maxSalary) {
+    salary = `от ${data.salary.minSalary} до ${data.salary.maxSalary} сом`;
+  } else if (data.salary?.minSalary) {
+    salary = `от ${data.salary.minSalary} сом`;
+  } else if (data.salary?.maxSalary) {
+    salary = `до ${data.salary.maxSalary} сом`;
   }
   return (
     <div className="VacancyCard" style={{ display: visible ? 'flex' : 'none' }}>
       {image}
       <Typography variant="h5" sx={VacancyCardStyle.title}>
-        {data.title}
+        {data.vacancyTitle}
       </Typography>
       <Typography sx={VacancyCardStyle.subTitle}>
         {data.company}, {data.city}
