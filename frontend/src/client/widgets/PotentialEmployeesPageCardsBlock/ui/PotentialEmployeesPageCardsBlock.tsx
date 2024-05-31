faddcc1e98fd6fb75e177e0b930a5272ac1482ba
Muradil Koychubekov <muradil.koychubekob@gmail.com>
2024-05-31 19:48:29 +0600

    added form

--- frontend/src/client/widgets/PotentialEmployeesPageCardsBlock/ui/PotentialEmployeesPageCardsBlock.tsx
+++ frontend/src/client/widgets/PotentialEmployeesPageCardsBlock/ui/PotentialEmployeesPageCardsBlock.tsx
@@ -2,7 +2,7 @@ import PotentialEmployeesPageCard from './PotentialEmployeesPageCard';
 import './PotentialEmployeesPageCardsBlock.css';
 import { useAppDispatch, useAppSelector } from '../../../../app/store/hooks';
 import { selectUsers, selectUsersLoading } from '../../../../admin/page/usersPanel/model/usersSlice';
-import React, { useEffect, useState } from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { getAllUser } from '../../../../admin/page/usersPanel/api/usersThunk';
 import { Loader } from '../../../../shared/loader';
 import { Box, TextField } from '@mui/material';
@@ -28,7 +28,8 @@ const PotentialEmployeesPageCardsBlock: React.FC<Props> = ({ slice }) => {
     dispatch(getAllUser());
   }, [dispatch]);
 
-  const searchHandle = async () => {
+  const searchHandle = async (event: FormEvent) => {
+    event.preventDefault();
     await dispatch(getAllUser(prof));
   };
 
@@ -43,31 +44,33 @@ const PotentialEmployeesPageCardsBlock: React.FC<Props> = ({ slice }) => {
   return (
     <div className="PotentialEmployeesPageCardsBlock__container">
       <h2 className="PotentialEmployeesPageCardsBlock__title">{slice.primary.titletext}</h2>
-      <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: '0 15px',
-        margin: '15px 0',
-      }}>
-        <TextField
-          fullWidth
-          value={prof}
-          onChange={event => setProf(event.target.value)}
-          autoComplete="new-email"
-          InputProps={{
-            style: { borderRadius: '30px' },
-          }}
-        />
-        <LoadingButton
-          loading={isLoading}
-          type="button"
-          onClick={searchHandle}
-          variant="contained"
-          sx={{ py: 2, backgroundColor: '#FFD700', borderRadius: '30px' }}
-        >
-          Искать
-        </LoadingButton>
-      </Box>
+      <form onSubmit={searchHandle}>
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '0 15px',
+          margin: '15px 0',
+        }}>
+          <TextField
+            fullWidth
+            value={prof}
+            onChange={event => setProf(event.target.value)}
+            autoComplete="new-email"
+            placeholder="Frontend developer...."
+            InputProps={{
+              style: { borderRadius: '30px' },
+            }}
+          />
+          <LoadingButton
+            loading={isLoading}
+            type="submit"
+            variant="contained"
+            sx={{ py: 2, backgroundColor: '#FFD700', borderRadius: '30px' }}
+          >
+            Искать
+          </LoadingButton>
+        </Box>
+      </form>
       {users.length === 0 ? (
         <p className="PotentialEmployeesPageCardsBlock__subtitle-no-cards">Нет сотрудников для отображения</p>
       ) : (
