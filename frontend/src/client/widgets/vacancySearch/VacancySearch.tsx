fb4416f2b0c332a9d837015360afa1c73fbe9543
Susanna Andreeva <sanadex90@gmail.com>
2024-06-02 20:40:39 +0900

    create VacanciesPage.tsx and add VacancySearch.tsx there

--- frontend/src/client/widgets/vacancySearch/VacancySearch.tsx
+++ frontend/src/client/widgets/vacancySearch/VacancySearch.tsx
@@ -1,47 +1,50 @@
-import { Box, Button, TextField } from '@mui/material';
-import React, { useState } from 'react';
-
-interface SearchQuery {
-  searchQuery: string;
-}
-
-const initialState: SearchQuery = {
-  searchQuery: '',
-};
+import { Box, TextField } from '@mui/material';
+import React, { FormEvent, useState } from 'react';
+import { LoadingButton } from '@mui/lab';
 
 interface Props {
-  onSearch: (searchQuery: string) => void;
+  onSearch: (vacancyTitle: string) => void;
+  isLoading: boolean;
 }
 
-const VacancySearch: React.FC<Props> = ({ onSearch }) => {
-  const [state, setState] = useState<SearchQuery>(initialState);
+const VacancySearch: React.FC<Props> = ({ onSearch, isLoading }) => {
+  const [vacancyTitle, setVacancyTitle] = useState('');
 
-  const submitFormHandler = (e: React.FormEvent) => {
-    e.preventDefault();
-    onSearch(state.searchQuery);
-  };
-
-  const inputChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    onSearch(vacancyTitle);
   };
 
   return (
-    <Box component="form" onSubmit={submitFormHandler} display="flex" alignItems="center" gap={2}>
-      <TextField
-        label="Search by company or profession"
-        variant="outlined"
-        name="searchQuery"
-        value={state.searchQuery}
-        onChange={inputChangeHandler}
-      />
-      <Button variant="contained" color="primary" type="submit">
-        Search
-      </Button>
-    </Box>
+    <form onSubmit={handleSubmit}>
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: '0 15px',
+          margin: '15px 0',
+        }}
+      >
+        <TextField
+          fullWidth
+          value={vacancyTitle}
+          onChange={(event) => setVacancyTitle(event.target.value)}
+          autoComplete="off"
+          placeholder="Введите название вакансии..."
+          InputProps={{
+            style: { borderRadius: '30px' },
+          }}
+        />
+        <LoadingButton
+          loading={isLoading}
+          type="submit"
+          variant="contained"
+          sx={{ py: 2, backgroundColor: '#FFD700', borderRadius: '30px' }}
+        >
+          Искать
+        </LoadingButton>
+      </Box>
+    </form>
   );
 };
 
