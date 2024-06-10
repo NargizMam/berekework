7ada752eac61f6cd9cb2c34c3a4e899575ef8bad
Susanna Andreeva <sanadex90@gmail.com>
2024-06-11 04:55:41 +0900

    Обновление VacancyBlock: добавлена функциональность для кнопки "смотреть еще"

--- frontend/src/client/widgets/vacancyBlock/ui/VacancyBlock.tsx
+++ frontend/src/client/widgets/vacancyBlock/ui/VacancyBlock.tsx
@@ -1,50 +1,60 @@
 import { CircularProgress } from '@mui/material';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../../app/store/hooks';
 import { getAllVacancyToCard } from '../../../../feachers/vacancy/vacancyThunk';
-import { VacancyCard } from '../../../../feachers/vacancyCard';
 import './VacancyBlock.css';
 import { selectVacancyToCards } from '../../../../feachers/vacancy/vacancySlice';
+import { VacancyCard } from '../../../../feachers/vacancyCard';
 
 interface IVacancyBlockProps {
-	slice: any;
+  slice: any;
 }
 
 export const VacancyBlock: React.FC<IVacancyBlockProps> = (data) => {
-	const dispatch = useAppDispatch();
-	const vacancyCard = useAppSelector(selectVacancyToCards);
-	
+  const dispatch = useAppDispatch();
+  const vacancyCards = useAppSelector(selectVacancyToCards);
+  const [visibleCount, setVisibleCount] = useState(6);
 
-	useEffect(() => {
-		dispatch(getAllVacancyToCard());
-	}, [dispatch]);
-	
-	const renderContent = () => {
-		if (!vacancyCard) {
-			return <CircularProgress sx={{margin: 'auto'}}/>;
-		}
-		
-		return (
-			<>
-				<div className='VacancyBlock__flex'>
-					{vacancyCard.map((data, index) => {
-						if (index < 6) {
-							return <VacancyCard key={index} data={data} visible={true} />;
-						}
-						return null;
-					})}
-				</div>
-			</>
-		);
-	};
-	
-	return (
-		<div className='vacancy_block'>
-			<h1 className='vacancy_block_title'>{data.slice.primary.vacancies_block_title}</h1>
-			<div>
-				{renderContent()}
-			</div>
-			<button className='vacancy_block_button'>{data.slice.primary.button_text_vacancies_block}</button>
-		</div>
-	);
+  useEffect(() => {
+    dispatch(getAllVacancyToCard());
+  }, [dispatch]);
+
+  const handleLoadMore = () => {
+    setVisibleCount((prevCount) => prevCount + 6);
+  };
+
+  const renderContent = () => {
+    if (!vacancyCards) {
+      return <CircularProgress sx={{ margin: 'auto' }} />;
+    }
+
+    return (
+      <>
+        <div className="VacancyBlock__flex">
+          {/*{vacancyCard.map((data, index) => {*/}
+          {/*  if (index < 6) {*/}
+          {/*    return <VacancyCard key={index} data={data} visible={true} />;*/}
+          {/*  }*/}
+          {/*  return null;*/}
+          {/*})}*/}
+          {vacancyCards.slice(0, visibleCount).map((data, index) => (
+            <VacancyCard key={index} data={data} visible={true} />
+          ))}
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <div className="vacancy_block">
+      <h1 className="vacancy_block_title">{data.slice.primary.vacancies_block_title}</h1>
+      <div>{renderContent()}</div>
+      {/*<button className="vacancy_block_button">{data.slice.primary.button_text_vacancies_block}</button>*/}
+      {vacancyCards && visibleCount < vacancyCards.length && (
+        <button className="vacancy_block_button" onClick={handleLoadMore}>
+          {data.slice.primary.button_text_vacancies_block}
+        </button>
+      )}
+    </div>
+  );
 };
