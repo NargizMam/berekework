399376db6f3236d71c7e567ef8b0784c1f3edf85
rdb03 <rdb03@mail.ru>
2024-06-11 15:28:03 +0600

    fixed code VacancyBlock.tsx

--- frontend/src/client/widgets/vacancyBlock/ui/VacancyBlock.tsx
+++ frontend/src/client/widgets/vacancyBlock/ui/VacancyBlock.tsx
@@ -1,5 +1,6 @@
 import { CircularProgress } from '@mui/material';
 import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../../../../app/store/hooks';
 import { getAllVacancyToCard } from '../../../../feachers/vacancy/vacancyThunk';
 import './VacancyBlock.css';
@@ -13,40 +14,48 @@ interface IVacancyBlockProps {
 export const VacancyBlock: React.FC<IVacancyBlockProps> = (data) => {
   const dispatch = useAppDispatch();
   const vacancyCards = useAppSelector(selectVacancyToCards);
-  const [visibleCount, setVisibleCount] = useState(6);
-
+  const [visibleVacancies, setVisibleVacancies] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  
   useEffect(() => {
     dispatch(getAllVacancyToCard());
   }, [dispatch]);
-
-  const handleLoadMore = () => {
-    setVisibleCount((prevCount) => prevCount + 6);
-  };
-
+  
+  useEffect(() => {
+    if (vacancyCards) {
+      setVisibleVacancies(vacancyCards.slice(-6));
+      setIsLoading(false);
+    }
+  }, [vacancyCards]);
+  
   const renderContent = () => {
-    if (!vacancyCards) {
+    if (isLoading) {
       return <CircularProgress sx={{ margin: 'auto' }} />;
     }
-
+    
+    if (visibleVacancies.length === 0) {
+      return <p className='not-vacancies-text'>Пока нет никаких вакансий</p>;
+    }
+    
     return (
       <>
         <div className="VacancyBlock__flex">
-          {vacancyCards.slice(0, visibleCount).map((data, index) => (
-            <VacancyCard key={index} data={data} visible={true} />
+          {visibleVacancies.map((vacancy, index) => (
+            <VacancyCard key={index} data={vacancy} visible={true} />
           ))}
         </div>
       </>
     );
   };
-
+  
   return (
     <div className="vacancy_block">
       <h1 className="vacancy_block_title">{data.slice.primary.vacancies_block_title}</h1>
       <div>{renderContent()}</div>
-      {vacancyCards && visibleCount < vacancyCards.length && (
-        <button className="vacancy_block_button" onClick={handleLoadMore}>
+      {vacancyCards && visibleVacancies.length < vacancyCards.length && visibleVacancies.length > 0 && (
+        <NavLink to='/vacancy' className="vacancy_block_button">
           {data.slice.primary.button_text_vacancies_block}
-        </button>
+        </NavLink>
       )}
     </div>
   );
