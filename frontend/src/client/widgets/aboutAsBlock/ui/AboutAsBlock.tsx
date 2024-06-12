d99e93b770521ca1906e5c205e1ddd5c2a79be06
rdb03 <rdb03@mail.ru>
2024-06-12 15:34:57 +0600

    finished pathing the links

--- frontend/src/client/widgets/aboutAsBlock/ui/AboutAsBlock.tsx
+++ frontend/src/client/widgets/aboutAsBlock/ui/AboutAsBlock.tsx
@@ -22,70 +22,70 @@ interface AboutUsInfoProps {
 }
 
 interface Props {
-  slice: AboutUsInfoProps;
+	slice: AboutUsInfoProps;
 }
 
 export const AboutUsBlock: React.FC<Props> = ({ slice }) => {
-  return (
-    <Box id='our-company' key={slice.id} sx={{ marginBottom: { xs: '7%', md: '161px' }, marginTop: { xs: '7%', md: '180px' } }}>
-      {slice.primary.aboutustitle && slice.primary.aboutustitle[0] && (
-        <Typography
-          variant="h4"
-          sx={{
-            fontSize: { xs: '1.5rem', sm: '2rem', md: '2.875rem' },
-            fontWeight: 700,
-            lineHeight: 1.3,
-            color: '#000',
-            marginBottom: { xs: '7%', md: '60px' },
-          }}
-        >
-          {slice.primary.aboutustitle[0].text}
-        </Typography>
-      )}
-
-      {slice.items.map((item, index) => (
-        <Box key={index} sx={{ marginBottom: { xs: '7%', md: '40px' } }}>
-          {item.aboutusdescription && item.aboutusdescription[0] && (
-            <Typography
-              component="span"
-              variant="body1"
-              sx={{
-                color: '#777',
-                fontSize: '1.125rem',
-                fontWeight: 500,
-                lineHeight: 1.44,
-              }}
-            >
-              {item.aboutusdescription && item.aboutusdescription[0] && (
-                <Typography
-                  component="span"
-                  variant="body1"
-                  sx={{
-                    fontSize: '1.25rem',
-                    fontWeight: 600,
-                    color: '#000',
-                    padding: 0,
-                    lineHeight: 1.1,
-                  }}
-                >
-                  {item.aboutusdescription[0].text}{' '}
-                </Typography>
-              )}
-              {index === 0 ? (
-                <Typography component="span" variant="body1">
-                  {item.aboutusdescription[1].text}
-                </Typography>
-              ) : (
-                <Box component="span" sx={{ display: 'block', marginTop: '10px' }}>
-                  <Typography component="span" variant="body1">
-                    {item.aboutusdescription[1].text}
-                  </Typography>
-                </Box>
-              )}
-            </Typography>
-          )}
-        </Box>
-      ))}
-    </Box>
-  );
+	return (
+		<Box id='about-us' key={slice.id} sx={{ marginBottom: { xs: '7%', md: '161px' }, marginTop: { xs: '7%', md: '180px' } }}>
+			{slice.primary.aboutustitle && slice.primary.aboutustitle[0] && (
+				<Typography
+					variant='h4'
+					sx={{
+						fontSize: { xs: '1.5rem', sm: '2rem', md: '2.875rem' },
+						fontWeight: 700,
+						lineHeight: 1.3,
+						color: '#000',
+						marginBottom: { xs: '7%', md: '60px' }
+					}}
+				>
+					{slice.primary.aboutustitle[0].text}
+				</Typography>
+			)}
+			
+			{slice.items.map((item, index) => (
+				<Box key={index} sx={{ marginBottom: { xs: '7%', md: '40px' } }}>
+					{item.aboutusdescription && item.aboutusdescription[0] && (
+						<Typography
+							component='span'
+							variant='body1'
+							sx={{
+								color: '#777',
+								fontSize: '1.125rem',
+								fontWeight: 500,
+								lineHeight: 1.44
+							}}
+						>
+							{item.aboutusdescription && item.aboutusdescription[0] && (
+								<Typography
+									component='span'
+									variant='body1'
+									sx={{
+										fontSize: '1.25rem',
+										fontWeight: 600,
+										color: '#000',
+										padding: 0,
+										lineHeight: 1.1
+									}}
+								>
+									{item.aboutusdescription[0].text}{' '}
+								</Typography>
+							)}
+							{index === 0 ? (
+								<Typography component='span' variant='body1'>
+									{item.aboutusdescription[1].text}
+								</Typography>
+							) : (
+								<Box component='span' sx={{ display: 'block', marginTop: '10px' }}>
+									<Typography component='span' variant='body1'>
+										{item.aboutusdescription[1].text}
+									</Typography>
+								</Box>
+							)}
+						</Typography>
+					)}
+				</Box>
+			))}
+		</Box>
+	);
 };
