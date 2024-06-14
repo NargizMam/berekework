6c5b5844134400ca8dd603760cfb917e227aa6bf
rdb03 <rdb03@mail.ru>
2024-06-14 13:10:55 +0600

    create success tests register

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -1,13 +1,8 @@
 const { I } = inject();
 
-Given('I have a defined step', () => {
-  // TODO: replace with your own step
-});
-
 Given('я нахожусь на странице {string}', (page: string) => {
   console.log(page);
   I.amOnPage(page);
-  I.wait(2);
 });
 
 Given('я ввожу {string} в поле {string}', (text: string, field: string) => {
