2b7032bd41df8aa78c2816936f230b24bfc10851
Kamila <kami.bayandinova@gmail.com>
2024-06-20 13:50:24 +0600

    KAN-187

--- frontend/src/client/page/employerProfile/ui/EmployerEdit.tsx
+++ frontend/src/client/page/employerProfile/ui/EmployerEdit.tsx
@@ -14,7 +14,8 @@ import {
 import { getExtension } from '../../../../feachers/checkExtensiion';
 import { Button, Grid, TextField } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
-
+import '../../../widgets/Applicant/ApplicantForm.css';
+import EditIcon from '@mui/icons-material/Edit';
 export interface EmployerInfoApiMutation {
   email: string;
   password: string;
@@ -158,187 +159,216 @@ export const EmployerEditPage = () => {
     }
   };
 
-
-
   return (
     <>
-      <form
+      <div
         style={{
-          margin: '30px auto',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          position: 'relative',
         }}
-        autoComplete="off"
-        onSubmit={handleCreateEmployer}
       >
-        <Grid container spacing={2}>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.email}
-              onChange={changeField}
-              name="email"
-              type="email"
-              id="standard-basic"
-              label="Email"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('email'))}
-              helperText={getFieldError('email')}
-            />
-          </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.companyName}
-              onChange={changeField}
-              name="companyName"
-              id="standard-basic"
-              label="Company name"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('companyName'))}
-              helperText={getFieldError('companyName')}
-            />
-          </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.industry}
-              onChange={changeField}
-              name="industry"
-              id="standard-basic"
-              label="industry"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('industry'))}
-              helperText={getFieldError('industry')}
-            />
-          </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.description}
-              onChange={changeField}
-              name="description"
-              id="standard-basic"
-              label="description"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('description'))}
-              helperText={getFieldError('description')}
-            />
-          </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.foundationYear}
-              onChange={changeField}
-              name="foundationYear"
-              type="number"
-              id="standard-basic"
-              label="Foundation Year"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('foundationYear'))}
-              helperText={getFieldError('foundationYear')}
-            />
-          </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.address}
-              onChange={changeField}
-              name="address"
-              id="standard-basic"
-              label="Address"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('address'))}
-              helperText={getFieldError('address')}
-            />
-          </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <TextField
-              value={state.contacts}
-              onChange={changeField}
-              name="contacts"
-              id="standard-basic"
-              label="Contacts"
-              variant="outlined"
-              required={true}
-              InputProps={{ style: inputStyle }}
-              error={Boolean(getFieldError('contacts'))}
-              helperText={getFieldError('contacts')}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <input
-              style={{ display: 'none' }}
-              type="file"
-              name="document"
-              onChange={changeFileFiled}
-              ref={documentSelect}
-            />
-            <Grid container direction="row" spacing={2} alignItems="center">
-              <Grid item xs>
-                <TextField
-                  disabled
-                  label="document"
-                  value={filename || ''}
-                  InputProps={{ style: inputStyle }}
-                  onClick={() => selectFile('document')}
-                  error={errorDocument}
-                  helperText={errorDocument ? 'PDF format!' : null}
-                />
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => selectFile('document')}>
-                  Browse
-                </Button>
-              </Grid>
-              {filename.length !== 0 && (
-                <Grid item>
-                  <Button variant="contained" onClick={clearDocumentField}>
-                    Clear
-                  </Button>
+        <div className="applicantContainer" style={{paddingTop: '20px', marginTop: '50px'}}>
+          <p className="profileTitle">Настройки профиля</p>
+          <Grid sx={{maxWidth: '850px', backgroundColor: '#ffff', padding: '20px', borderRadius: '30px'}}>
+            <form
+              style={{
+                margin: '30px auto',
+              }}
+              autoComplete="off"
+              onSubmit={handleCreateEmployer}
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.email}
+                    onChange={changeField}
+                    name="email"
+                    type="email"
+                    id="standard-basic"
+                    label="Email"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('email'))}
+                    helperText={getFieldError('email')}
+                  />
                 </Grid>
-              )}
-            </Grid>
-          </Grid>
-          <Grid item xs={12}>
-            <input style={{ display: 'none' }} type="file" name="logo" onChange={changeFileFiled} ref={imageSelect} />
-            <Grid container direction="row" spacing={2} alignItems="center">
-              <Grid item xs>
-                <TextField
-                  disabled
-                  label="Logo"
-                  InputProps={{ style: inputStyle }}
-                  value={filenameImage || ''}
-                  onClick={() => selectFile('image')}
-                  error={Boolean(getFieldError('logo'))}
-                  helperText={getFieldError('logo')}
-                />
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={() => selectFile('image')}>
-                  Browse
-                </Button>
-              </Grid>
-              {filenameImage && filenameImage.toString().length !== 0 && (
-                <Grid item>
-                  <Button variant="contained" onClick={clearDocumentField}>
-                    Clear
-                  </Button>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.companyName}
+                    onChange={changeField}
+                    name="companyName"
+                    id="standard-basic"
+                    label="Company name"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('companyName'))}
+                    helperText={getFieldError('companyName')}
+                  />
                 </Grid>
-              )}
-            </Grid>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.industry}
+                    onChange={changeField}
+                    name="industry"
+                    id="standard-basic"
+                    label="industry"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('industry'))}
+                    helperText={getFieldError('industry')}
+                  />
+                </Grid>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.description}
+                    onChange={changeField}
+                    name="description"
+                    id="standard-basic"
+                    label="description"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('description'))}
+                    helperText={getFieldError('description')}
+                  />
+                </Grid>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.foundationYear}
+                    onChange={changeField}
+                    name="foundationYear"
+                    type="number"
+                    id="standard-basic"
+                    label="Foundation Year"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('foundationYear'))}
+                    helperText={getFieldError('foundationYear')}
+                  />
+                </Grid>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.address}
+                    onChange={changeField}
+                    name="address"
+                    id="standard-basic"
+                    label="Address"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('address'))}
+                    helperText={getFieldError('address')}
+                  />
+                </Grid>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <TextField
+                    style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                    value={state.contacts}
+                    onChange={changeField}
+                    name="contacts"
+                    id="standard-basic"
+                    label="Contacts"
+                    variant="outlined"
+                    required={true}
+                    InputProps={{style: inputStyle}}
+                    error={Boolean(getFieldError('contacts'))}
+                    helperText={getFieldError('contacts')}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <input
+                    style={{display: 'none'}}
+                    type="file"
+                    name="document"
+                    onChange={changeFileFiled}
+                    ref={documentSelect}
+                  />
+                  <Grid container direction="row" spacing={2} alignItems="center">
+                    <Grid item xs>
+                      <TextField
+                        style={{backgroundColor: '#ffff', borderRadius: '30px'}}
+                        disabled
+                        label="document"
+                        value={filename || ''}
+                        InputProps={{style: inputStyle}}
+                        onClick={() => selectFile('document')}
+                        error={errorDocument}
+                        helperText={errorDocument ? 'PDF format!' : null}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Button variant="contained" onClick={() => selectFile('document')}>
+                        Browse
+                      </Button>
+                    </Grid>
+                    {filename.length !== 0 && (
+                      <Grid item>
+                        <Button variant="contained" onClick={clearDocumentField}>
+                          Clear
+                        </Button>
+                      </Grid>
+                    )}
+                  </Grid>
+                </Grid>
+                <Grid item xs={12}>
+                  <input style={{display: 'none'}} type="file" name="logo" onChange={changeFileFiled}
+                         ref={imageSelect}/>
+                  <Grid container direction="row" spacing={2} alignItems="center">
+                    <Grid item xs>
+                      <TextField
+                        disabled
+                        label="Logo"
+                        InputProps={{style: inputStyle}}
+                        value={filenameImage || ''}
+                        onClick={() => selectFile('image')}
+                        error={Boolean(getFieldError('logo'))}
+                        helperText={getFieldError('logo')}
+                      />
+                    </Grid>
+                    <Grid item>
+                      <Button variant="contained" onClick={() => selectFile('image')}>
+                        Browse
+                      </Button>
+                    </Grid>
+                    {filenameImage && filenameImage.toString().length !== 0 && (
+                      <Grid item>
+                        <Button variant="contained" onClick={clearDocumentField}>
+                          Clear
+                        </Button>
+                      </Grid>
+                    )}
+                  </Grid>
+                </Grid>
+                <Grid item xs={registerStyle ? 12 : 6}>
+                  <LoadingButton
+                    className='sendBtn'
+                    loading={loading}
+                    type="submit"
+                    variant="contained"
+                    sx={{width: '100%'}}
+                  >
+                    <EditIcon style={{color: '#FFFFFF', marginRight: '20px'}}/>
+                    Редактировать профиль
+                  </LoadingButton>
+                </Grid>
+              </Grid>
+            </form>
           </Grid>
-          <Grid item xs={registerStyle ? 12 : 6}>
-            <LoadingButton loading={loading} type="submit" variant="contained" sx={{ width: '100%' }}>
-              Create
-            </LoadingButton>
-          </Grid>
-        </Grid>
-      </form>
+        </div>
+      </div>
+
     </>
   );
 };
