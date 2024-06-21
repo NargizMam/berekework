0495c9a13cacc8d569321002f39f24dd5d4dfeed
Muradil <muradil.koychubekob@gmail.com>
2024-06-21 20:01:39 +0600

    start test

--- frontend/src/app/store/store.ts
+++ frontend/src/app/store/store.ts
@@ -41,7 +41,20 @@ export const store = configureStore({
     }),
 });
 
+export const setupStore = (preloadedState?: Partial<RootState>) =>
+  configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, PAUSE, PERSIST, REHYDRATE, PURGE, REGISTER],
+        },
+      }),
+    preloadedState,
+  });
+
 export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = typeof store.dispatch;
