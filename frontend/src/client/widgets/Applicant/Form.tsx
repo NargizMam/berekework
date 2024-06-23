4cc4eae34ba3215994158baf7a666dd653ffa341
Kamila <kami.bayandinova@gmail.com>
2024-06-23 16:23:53 +0600

    KAN-198 исправлены стрелки в селекте и адаптивность у формы

--- frontend/src/client/widgets/Applicant/Form.tsx
+++ frontend/src/client/widgets/Applicant/Form.tsx
@@ -46,107 +46,122 @@ const Form: React.FC<Props> = ({ loading, fields, state, submitFormHandler, inpu
           </div>
           <Grid item xs>
             <label className="labelForField" htmlFor="patronymic">Отчество</label>
-            <input className="field" id="patronymic" value={state.patronymic} onChange={inputChangeHandler}
+            <input className="applicantField" id="patronymic" value={state.patronymic} onChange={inputChangeHandler}
                    name="patronymic" />
           </Grid>
           <div className="inputContainer">
             <Grid item xs={7}>
               <label className="labelForField" htmlFor="gender">Пол</label>
-              <select className="applicantField" onChange={inputChangeHandler} id="gender" value={state.gender}
-                      name="gender" required>
-                <option className="menuItem" value="">Не указан</option>
-                <option className="menuItem" value="жен">Женский</option>
-                <option className="menuItem" value="муж">Мужской</option>
-              </select>
+              <div className='select-wrapper'>
+                <select className="applicantField" onChange={inputChangeHandler} id="gender" value={state.gender}
+                        name="gender" required>
+                  <option className="menuItem" value="">Не указан</option>
+                  <option className="menuItem" value="жен">Женский</option>
+                  <option className="menuItem" value="муж">Мужской</option>
+                </select>
+              </div>
+
             </Grid>
             <Grid item xs>
               <label className="labelForField" htmlFor="dateOfBirth">Дата рождения</label>
-              <input type={'date'} className="field" id="dateOfBirth" value={state.dateOfBirth}
-                     onChange={inputChangeHandler} name="dateOfBirth" required min="1950-01-01" />
+              <input type={'date'} className="applicantField" id="dateOfBirth" value={state.dateOfBirth}
+                     onChange={inputChangeHandler} name="dateOfBirth" required min="1950-01-01"/>
             </Grid>
           </div>
           <div className="inputContainer">
             <Grid item xs>
               <label className="labelForField" htmlFor="country">Страна</label>
-              <select className="applicantField" id="country" value={state.country} onChange={inputChangeHandler}
-                      name="country" required>
-                <option className="menuItem" value="">Выберите страну</option>
-                {countries.map((item, index) => (
-                  <option className="menuItem" key={`country-${index}`} value={item.name}>{item.name}</option>
-                ))}
-              </select>
+              <div className="select-wrapper">
+                <select className="applicantField" id="country" value={state.country} onChange={inputChangeHandler}
+                        name="country" required>
+                  <option className="menuItem" value="">Выберите страну</option>
+                  {countries.map((item, index) => (
+                    <option className="menuItem" key={`country-${index}`} value={item.name}>{item.name}</option>
+                  ))}
+                </select>
+              </div>
             </Grid>
             <Grid item xs>
               <label className="labelForField" htmlFor="city">Город</label>
-              <select className="applicantField" id="city" value={state.city} onChange={inputChangeHandler} name="city"
-                      required>
-                <option className="menuItem" value="">Выберите город</option>
-                {state.city ? <option className="menuItem">{state.city}</option> : null}
-                <option className="menuItem" value="">
-                  Не указан
-                </option>
-                {countries.map((country) => {
-                  if (state.country === country.name) {
-                    return country.cities.map((city, index) => (
-                      <option key={`city-${index}`} className="menuItem">
-                        {city}
-                      </option>
-                    ));
-                  }
-                  return null;
-                })}
-              </select>
+              <div className="select-wrapper">
+                <select className="applicantField" id="city" value={state.city} onChange={inputChangeHandler}
+                        name="city"
+                        required>
+                  <option className="menuItem" value="">Выберите город</option>
+                  {state.city ? <option className="menuItem">{state.city}</option> : null}
+                  <option className="menuItem" value="">
+                    Не указан
+                  </option>
+                  {countries.map((country) => {
+                    if (state.country === country.name) {
+                      return country.cities.map((city, index) => (
+                        <option key={`city-${index}`} className="menuItem">
+                          {city}
+                        </option>
+                      ));
+                    }
+                    return null;
+                  })}
+                </select>
+              </div>
             </Grid>
           </div>
           <Grid item xs>
             <label className="labelForField" htmlFor="education">Образование</label>
-            <select className="applicantField" id="education" value={state.education} onChange={inputChangeHandler}
-                    name="education" required>
-              <option value="">Не указано</option>
-              {educationTypes.map((educationType, index) => (
-                <option key={index} value={educationType}>{educationType}</option>
-              ))}
-            </select>
+            <div className="select-wrapper">
+              <select className="applicantField" id="education" value={state.education} onChange={inputChangeHandler}
+                      name="education" required>
+                <option value="">Не указано</option>
+                {educationTypes.map((educationType, index) => (
+                  <option key={index} value={educationType}>{educationType}</option>
+                ))}
+              </select>
+            </div>
           </Grid>
           <Grid item xs>
-            <label className="labelForField" htmlFor="aboutMe">О себе</label>
+          <label className="labelForField" htmlFor="aboutMe">О себе</label>
             <textarea className="applicantField" id="aboutMe" value={state.aboutMe} onChange={inputChangeHandler}
                       name="aboutMe" required />
           </Grid>
           <Grid item xs>
-            <Grid item xs ml={6} mb={2}>
+            <Grid item xs mb={2}>
               <Button variant="contained"
-                      sx={{ marginTop: '10px', backgroundColor: '#0866FF', color: '#ffff', borderRadius: '30px' }}
+                      sx={{ marginTop: '10px', backgroundColor: '#0866FF', color: '#ffff', borderRadius: '30px', width: "250px" }}
                       onClick={addField}>
                 Добавить опыт работы
               </Button>
+
             </Grid>
             {fields.map((field) => field)}
           </Grid>
           <div className="inputContainer">
             <Grid item xs>
               <label className="labelForField" htmlFor="preferredJob">Ищу работу в сфере:</label>
-              <select className="applicantField" id="preferredJob" value={state.preferredJob}
-                      onChange={inputChangeHandler} name="preferredJob" required>
-                <option className="menuItem" value="">Выберите профессию</option>
-                {jobs.map((job) => (
-                  <option key={job} value={job}>{job}</option>
-                ))}
-              </select>
+              <div className="select-wrapper">
+                <select className="applicantField" id="preferredJob" value={state.preferredJob}
+                        onChange={inputChangeHandler} name="preferredJob" required>
+                  <option className="menuItem" value="">Выберите профессию</option>
+                  {jobs.map((job) => (
+                    <option key={job} value={job}>{job}</option>
+                  ))}
+                </select>
+              </div>
             </Grid>
             <Grid item xs>
               <label className="labelForField" htmlFor="preferredCity">Предпочитаемый город для работы</label>
-              <select className="applicantField" id="preferredCity" value={state.preferredCity}
-                      onChange={inputChangeHandler} name="preferredCity" required>
-                <option className="menuItem" value="">Выберите город</option>
-                {countries.map((country) => (
-                  country.cities.map((city, index) => (
-                    <option key={`preferred-city-${index}`} className="menuItem">
-                      {city}
-                    </option>
-                  ))
-                ))}
-              </select>
+              <div className="select-wrapper">
+                <select className="applicantField" id="preferredCity" value={state.preferredCity}
+                        onChange={inputChangeHandler} name="preferredCity" required>
+                  <option className="menuItem" value="">Выберите город</option>
+                  {countries.map((country) => (
+                    country.cities.map((city, index) => (
+                      <option key={`preferred-city-${index}`} className="menuItem">
+                        {city}
+                      </option>
+                    ))
+                  ))}
+                </select>
+              </div>
             </Grid>
           </div>
           <div className="inputContainer">
@@ -176,9 +191,9 @@ const Form: React.FC<Props> = ({ loading, fields, state, submitFormHandler, inpu
           disabled={allFieldsEmpty}
         >
           Сохранить изменения
-        </LoadingButton>;
+        </LoadingButton>
         <Button color="primary" variant="contained" className="sendBtn"
-                onClick={() => navigate('/userProfile')}>Назад</Button>;
+                onClick={() => navigate('/userProfile')}>Назад</Button>
       </form>
     </div>
   )
