85635553b886f9fedb9433bc215aea30e4109896
rdb03 <rdb03@mail.ru>
2024-06-25 18:52:11 +0600

    added two acceptance tests for employer registration and going to the jobs page

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -16,3 +16,89 @@ When('я нажимаю на кнопку {string}', (button: string) => {
 When('я вижу текст {string}', (text: string) => {
   I.see(text);
 });
+
+Given('я нахожусь на главной странице {string}', (page: string) => {
+  I.amOnPage(page);
+});
+
+Then('я вижу ссылку {string} в футере', (linkText: string) => {
+  I.scrollTo('footer');
+  I.see(linkText, 'footer');
+});
+
+When('я нажимаю на ссылку {string}', (linkText: string) => {
+  I.click(linkText, 'footer');
+});
+
+Then('я перехожу к блоку на странице {string}', (url: string) => {
+  I.seeInCurrentUrl(url);
+  I.scrollTo(url.split('#')[1]);
+  I.seeElement(url.split('#')[1]);
+});
+
+Then('я вижу заголовок {string} на странице', (heading: string) => {
+  I.see(heading);
+});
+
+Then('я вижу заголовок {string} на странице', (heading: string) => {
+  I.see(heading);
+});
+
+Then('я вижу ссылку {string} в футере', (linkText: string) => {
+  I.scrollTo('footer');
+  I.see(linkText, 'footer');
+});
+
+When('я нажимаю на ссылку {string}', (linkText: string) => {
+  I.click(linkText, 'footer');
+});
+
+Then('я перехожу к блоку на странице {string}', (url: string) => {
+  I.seeInCurrentUrl(url);
+  I.waitForElement(`#${url.split('#')[1]}`, 5); // ожидание до 5 секунд
+  I.scrollTo(`#${url.split('#')[1]}`);
+  I.seeElement(`#${url.split('#')[1]}`);
+});
+
+Then('я вижу заголовок {string} на странице', (heading: string) => {
+  I.see(heading);
+});
+
+When('я перехожу на страницу {string}', (page: string) => {
+  I.amOnPage(page);
+});
+
+Then('я перехожу на страницу {string}', (url: string) => {
+  I.seeInCurrentUrl(url);
+  I.waitForElement('h1', 5);
+  I.see('Последние вакансии', 'h1');
+});
+
+Given('я нахожусь на странице {string}', (page: string) => {
+  I.amOnPage(page);
+});
+
+When('я нажимаю на кнопку {string}', (button: string) => {
+  I.click(button);
+});
+
+When('я вижу текст {string}', (text: string) => {
+  I.see(text);
+});
+
+Given('я нахожусь на главной странице {string}', (page: string) => {
+  I.amOnPage(page);
+});
+
+Then('я вижу ссылку {string} в блоке вакансий', (linkText: string) => {
+  I.scrollTo('.vacancy_block');
+  I.see(linkText, '.vacancy_block');
+});
+
+When('я нажимаю на ссылку {string}', (linkText: string) => {
+  I.click(linkText, '.vacancy_block');
+});
+
+Then('я перехожу на страницу {string}', (url: string) => {
+  I.seeInCurrentUrl(url);
+});
