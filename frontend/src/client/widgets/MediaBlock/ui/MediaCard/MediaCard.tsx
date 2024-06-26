93d7fea98799b922aaf09c2a84be3f530f9bcc70
Susanna Andreeva <sanadex90@gmail.com>
2024-06-26 17:12:00 +0900

    fix VideoBlock.tsx and styles

--- frontend/src/client/widgets/MediaBlock/ui/MediaCard/MediaCard.tsx
+++ frontend/src/client/widgets/MediaBlock/ui/MediaCard/MediaCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Box, CardMedia } from '@mui/material';
 import getMediaCardStyle from './getMediaCardStyle';
-import iconPlay from '../../images/icon-play.png';
 
 export interface MediaCardApiData {
   index: number;
@@ -10,47 +9,29 @@ export interface MediaCardApiData {
     alt: string;
   };
   video?: {
-    embed_url: string;
-    thumbnail_url: string;
-    title: string;
-    // html: string;
+    html: string;
   };
   mediaCardsLength: number;
-  onClick: (index: number) => void;
+  onClick?: (index: number) => void;
 }
 
 const MediaCard: React.FC<MediaCardApiData> = ({ index, image, video, mediaCardsLength, onClick }) => {
   const MediaCardStyle = getMediaCardStyle(mediaCardsLength);
 
   const imageUrl = image ? image.url : undefined;
-  const videoUrl = video ? video.embed_url : undefined;
-  const videoThumbnailUrl = video ? video.thumbnail_url : undefined;
+  const videoHtml = video ? video.html : undefined;
 
   const mediaElement = (
-    <Box sx={MediaCardStyle.card} onClick={() => onClick(index)}>
+    <Box sx={MediaCardStyle.card} onClick={onClick ? () => onClick(index) : undefined}>
       {imageUrl && (
-        <CardMedia sx={MediaCardStyle.image} component="img" image={imageUrl} alt={image?.alt || video?.title || ''} />
+        <CardMedia sx={MediaCardStyle.image} component='img' image={imageUrl} alt={image?.alt} />
       )}
-      {videoUrl && videoThumbnailUrl && (
-        <>
-          <CardMedia sx={MediaCardStyle.image} component="img" image={videoThumbnailUrl} alt={video?.title || ''} />
-          <Box sx={MediaCardStyle.iconPlayWrapper}>
-            <img src={iconPlay} alt="play" />
-          </Box>
-        </>
-      )}
+      {videoHtml && (
+          <Box sx={MediaCardStyle.videoWrapper} dangerouslySetInnerHTML={{ __html: videoHtml }} />
+        )}
     </Box>
   );
 
-  // const mediaElement = video?.html ? (
-  //   <Box sx={MediaCardStyle.card} onClick={() => onClick(index)}>
-  //     <Box sx={MediaCardStyle.videoWrapper} dangerouslySetInnerHTML={{ __html: video.html }} />
-  //   </Box>
-  // ) : (
-  //   <Box sx={MediaCardStyle.card} onClick={() => onClick(index)}>
-  //     {image && <CardMedia component="img" image={image.url} alt={image.alt || ''} sx={MediaCardStyle.image} />}
-  //   </Box>
-  // );
   return <>{mediaElement}</>;
 };
 
--- frontend/src/client/widgets/MediaBlock/ui/VideoBlock.tsx
+++ frontend/src/client/widgets/MediaBlock/ui/VideoBlock.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { Navigation } from 'swiper/modules';
 import MediaCard, { MediaCardApiData } from './MediaCard/MediaCard';
 import MediaBlockStyle from './MediaBlock-style';
-import FsLightbox from 'fslightbox-react';
 import arrowLeft from '../../../../shared/assets/arrow-left.png';
 import arrowRight from '../../../../shared/assets/arrow-right.png';
 import './SwiperNavigation.css';
@@ -25,13 +24,6 @@ interface Props {
 }
 
 export const VideoBlock: React.FC<Props> = ({ slice }) => {
-  const [toggler, setToggler] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const openLightbox = (index: number) => {
-    setCurrentIndex(index);
-    setToggler(!toggler);
-  };
 
   if (!slice.items || slice.items.length === 0) {
     return (
@@ -42,8 +34,6 @@ export const VideoBlock: React.FC<Props> = ({ slice }) => {
   }
 
   const showNavigation = slice.items.length > 2;
-
-  const sources = slice.items.map((item) => item.video?.embed_url || '').filter((url): url is string => Boolean(url));
 
   return (
     <Box sx={MediaBlockStyle.videoContainer}>
@@ -70,7 +60,6 @@ export const VideoBlock: React.FC<Props> = ({ slice }) => {
             index={0}
             video={slice.items[0].video}
             mediaCardsLength={slice.items.length}
-            onClick={() => openLightbox(0)}
           />
         ) : (
           <Swiper
@@ -93,13 +82,11 @@ export const VideoBlock: React.FC<Props> = ({ slice }) => {
                   index={index}
                   video={item.video}
                   mediaCardsLength={slice.items.length}
-                  onClick={() => openLightbox(index)}
                 />
               </SwiperSlide>
             ))}
           </Swiper>
         )}
-        <FsLightbox toggler={toggler} sources={sources} slide={currentIndex + 1} />
       </Box>
     </Box>
   );
